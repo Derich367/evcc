786432974b90b54eaea24fca69143e739f3cd227
Derich367 <9803627+Derich367@users.noreply.github.com>
2025-08-21 22:01:17 +0200

    Fix lint

--- util/sponsor/auth.go
+++ util/sponsor/auth.go
@@ -107,10 +107,10 @@ type sponsorStatus struct {
 // Status returns the sponsorship status
 func Status() sponsorStatus {
 	return sponsorStatus{
-    	Name:        "Sponsor",
-        ExpiresAt:   time.Date(2050, time.January, 1, 0, 0, 0, 0, time.UTC),
-        ExpiresSoon: false,
-		Token:	     redactToken("Token"),
-		FromYaml:    true,
-    }
+		Name:        "Sponsor",
+		ExpiresAt:   time.Date(2050, time.January, 1, 0, 0, 0, 0, time.UTC),
+		ExpiresSoon: false,
+		Token:       redactToken(Token),
+		FromYaml:    fromYaml,
+	}
 }
